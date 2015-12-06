461cc824198f49c9f9205b8f8665ba4cfe01d827
gunderm2 <tomichaelpg@gmail.com>
2015-12-06 09:06:06 -0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
 	// the database url to connect
-    url : 'mongodb://app_user:Password#1@ds057244.mongolab.com:57244/mean-development'
+    url : 'mongodb://<app_user>:<Password>@ds<URI>:<port>/<db-name>'
 }
